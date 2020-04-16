e0902f44392dabea966f57f25f3e348b000b44b1
chan kim <siamore9724@gmail.com>
2020-04-16 15:30:25 +0900

    4/16 part1.30 facebook connect resolvers

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -23,9 +23,9 @@ const BCRYPT_ROUNDS = 10;
 class User extends BaseEntity {
   @PrimaryGeneratedColumn() id: number;
 
-  @Column({ type: "text", unique: true })
+  @Column({ type: "text", nullable: true })
   @IsEmail()
-  email: string;
+  email: string | null;
 
   @Column({ type: "boolean", default: false })
   verifiedEmail: boolean;
@@ -36,7 +36,7 @@ class User extends BaseEntity {
   @Column({ type: "text" })
   lastName: string;
 
-  @Column({ type: "int" })
+  @Column({ type: "int", nullable: true })
   age: number;
 
   @Column({ type: "text" })
@@ -69,6 +69,9 @@ class User extends BaseEntity {
   @Column({ type: "double precision", default: 0 })
   lastOrientation: number;
   
+  @Column({ type: "text", nullable: true })
+  fbId: string;
+
   @ManyToOne(type => Chat, chat => chat.participants)
   chat: Chat;
 
